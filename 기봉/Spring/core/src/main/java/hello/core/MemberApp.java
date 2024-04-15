f3eb050765e61ceb6e6f7e5be76d2f017fa74bb6
rohgibong <rohgibong@gmail.com>
2024-04-15 22:58:36 +0900

    sec03 update

--- 기봉/Spring/core/src/main/java/hello/core/MemberApp.java
+++ 기봉/Spring/core/src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // @Bean 붙은걸 다 spring컨테이너 안에 넣어서 관리해줌 (applicationContext)
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 메소드이름, 타입
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- 기봉/Spring/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ 기봉/Spring/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    // 테스트 실행 전에 무조건 실행되는 코드
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    //MemberService memberService = new MemberServiceImpl();
     @Test
     void join(){
         //given (~~한 게 주어졌을 때)
