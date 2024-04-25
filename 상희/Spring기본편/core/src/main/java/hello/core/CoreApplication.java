145402cf12987d64f757038580e7e919d06ab431
한상희 <tkdgml822@gmail.com>
2024-04-25 13:03:33 +0900

    5주차

--- 상희/Spring기본편/core/src/main/java/hello/core/CoreApplication.java
+++ 상희/Spring기본편/core/src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // (@ComponentScan이 기본적으로 들어있다.)
+@SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
---
+++ 상희/Spring기본편/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,78 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 프로토타입 빈을 사용하는 곳 : 매번 사용할 때 마다 의존관계 주입이 완료된 새로운 객체가 필요하면 사용하면 된다. 실무에서는 직접 사용하는 일은 매우 드물다.
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFined() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+
+
+        // javax.inject.Provider은 기능이 단순하고 단위테스트를 만들거나 mock 코드를 만들기는 훨씩 쉬워진다.
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
