bec6f355a9187f6b66d09d5ed695406f272475ff
GIbong Roh <rohgibong@gmail.com>
2024-04-28 23:28:43 +0900

    sec04 update

--- 기봉/Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 기봉/Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
