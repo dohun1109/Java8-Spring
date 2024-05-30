d09bfb365ea4d2485cc8f5e6897bf2c9b38375ec
rohgibong <rohgibong@gmail.com>
2024-05-30 16:19:17 +0900

    이것이 백엔드 개발이다 예제

--- 기봉/Spring/hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
+++ 기봉/Spring/hanbit/src/main/java/kr/co/hanbit/SimpleRestController.java
@@ -1,18 +1,14 @@
 package kr.co.hanbit;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
 
-    @RequestMapping("/")
-    public String hello(){
-        return "Hello <strong>Backend</strong>";
-    }
-
-    @RequestMapping("/bye")
-    public String bye(){
-        return "Bye";
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title, @RequestParam("content") String content){
+        return String.format("title=%s / content=%s", title, content);
     }
 }
