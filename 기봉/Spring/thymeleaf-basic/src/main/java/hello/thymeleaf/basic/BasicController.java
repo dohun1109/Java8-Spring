1c607a3ad9ec42bb6fada7a3f91934cdd2aa0600
rohgibong <rohgibong@gmail.com>
2024-06-07 21:18:33 +0900

    MVC2 sec01 update

--- 기봉/Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 기봉/Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -105,6 +105,31 @@ public class BasicController {
         return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
