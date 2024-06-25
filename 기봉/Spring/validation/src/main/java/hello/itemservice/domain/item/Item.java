f73f93abe7d3544cd568b2efcce527de40d54c85
rohgibong <rohgibong@gmail.com>
2024-06-25 20:54:37 +0900

    MVC2 sec05 update

--- 기봉/Spring/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 기봉/Spring/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,25 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
