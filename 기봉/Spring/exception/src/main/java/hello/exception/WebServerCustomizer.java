9529746d72b27780086462dbb9860ce68eabdfae
rohgibong <rohgibong@gmail.com>
2024-07-13 03:53:21 +0900

    MVC2 sec09 update

--- 기봉/Spring/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ 기봉/Spring/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -16,6 +16,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
 
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
+
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
 }
