6ae3e9777b29e8d2675d39ed863b999c86753518
rohgibong <rohgibong@gmail.com>
2024-07-17 15:10:52 +0900

    JPA_basic sec03 update

---
+++ 기봉/Spring/ex1-hello-jpa/src/main/java/jpa_basic/ex1_hello_jpa/Ex1HelloJpaApplication.java
@@ -0,0 +1,13 @@
+package jpa_basic.ex1_hello_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1HelloJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1HelloJpaApplication.class, args);
+	}
+
+}
---
+++ 기봉/Spring/ex1-hello-jpa/src/test/java/jpa_basic/ex1_hello_jpa/Ex1HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa_basic.ex1_hello_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1HelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
