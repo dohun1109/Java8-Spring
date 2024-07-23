6a1443b474c12eba7cf3d20df1e29ba6359ce4a3
rohgibong <rohgibong@gmail.com>
2024-07-23 22:14:27 +0900

    JPA_basic sec10 update

---
+++ 기봉/Spring/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,37 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            tx.commit();
+        } catch(Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        } finally{
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
