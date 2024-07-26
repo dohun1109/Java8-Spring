64fc41ea651906d5a08e46d244a5bb12ee0efe8c
rohgibong <rohgibong@gmail.com>
2024-07-26 19:59:31 +0900

    JPA_use_1 sec05 update

---
+++ 기봉/Spring/newJpaShop/src/main/java/jpabook/newJpaShop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.newJpaShop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.newJpaShop.domain.Item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+            .getResultList();
+    }
+
+
+}
